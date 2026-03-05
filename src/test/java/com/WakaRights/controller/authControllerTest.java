96a86ee8933e758d769e9f2a7640b96c92f0c54b
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-03-05 04:26:24 +0100

    first commit

---
+++ src/test/java/com/WakaRights/controller/authControllerTest.java
@@ -0,0 +1,45 @@
+package com.WakaRights.controller;
+
+
+import com.WakaRights.security.SecurityConfig;
+import com.WakaRights.service.AuthService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthController.class)
+@Import(SecurityConfig.class)
+public class authControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthService authService;
+
+    @Test
+    void registerSuccess() throws Exception {
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {"email":"test@mail.com","password":"123456"}
+                        """))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void uploadEvidence_badRequest() throws Exception {
+        mockMvc.perform(post("/api/evidence")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isBadRequest());
+    }
+
+}
